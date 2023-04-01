3c3bcb4ba190e8a047fb6335ee1dcbcb2e526b0d
BensonLiu06 <itsbensonliu@gmail.com>
2023-03-31 22:04:52 -0400

    Added descriptions and generating user file

--- Benson Liu Customer Information System.py
+++ Benson Liu Customer Information System.py
@@ -25,32 +25,34 @@ def enterCustomerInfo(): #Customer inputs their info
 
     
 
-    firstName = input("Enter your First Name: ")
-    lastName = input("Enter your Last Name: ")
-    city = input("Enter what city you are from: ")
+    firstName = input("Enter your First Name: ") #inputs their first name
+    lastName = input("Enter your Last Name: ") #inputs their last name
+    city = input("Enter what city you are from: ") #inputs their City
     while True:
-        postalCode = input("Enter Postal Code: ")
-        if len(postalCode) >= 3 and validatePostalCode(postalCode):
+        postalCode = input("Enter Postal Code: ") #inputs their postal code
+        if len(postalCode) >= 3 and validatePostalCode(postalCode): #verifys whether the input is 3 letters/numbers or more and if it passes the validation check
             break
-        elif len(postalCode) < 3:
+        elif len(postalCode) < 3: #if the postal code is less than 3 letters/numbers it will print invalid and ask for a valid input
             print("invalid Postal Code, please try again")
     
     while True:
-        creditCard=input("Credit Card number: ")
-        if len(creditCard) >= 9 and validateCreditCard(creditCard):
+        creditCard=input("Credit Card number: ") #inputs Credit Card
+        if len(creditCard) >= 9 and validateCreditCard(creditCard): #verifys if the input is more than or equal to 9 digits and checks if it passes the luhn algorithm credit card verification.
             break
-        elif len(creditCard) < 9:
+        elif len(creditCard) < 9: #if credit card has less than 9 digits, it will print invalid and ask for another input
             print(" Invalid Credit Card Number, please try again")
     
-    
+    # appends to a temporary file, if it exists, it will append, if it doesn't it will create a new one
     f = open("CustomerData.csv", "a")
 
+    # reads the file, and gives an ID to every line starting at 1.
     fileRead= open("customerData.csv", "r")
     id=1
     for line in fileRead.readlines():
         id += 1
 
-    f.write(f" {firstName} | {lastName} | {city} | {postalCode} | {creditCard} | {id}\n")
+    # writes the users input and ID in the file.
+    f.write(f"{firstName} | {lastName} | {city} | {postalCode} | {creditCard} | {id}\n")
     f.close()
 
 ''' 
@@ -61,22 +63,22 @@ def enterCustomerInfo(): #Customer inputs their info
 '''
 def validatePostalCode(postalCode): #determines whether the postal code is valid
     try:
-        if len(postalCode) < 3:
+        if len(postalCode) < 3: #checks if the postal code is less than 3 digits
             return False
         
 
         with open("postal_codes.csv", encoding='utf8', errors='ignore') as file:
             for line in file: #CHANGE VARIABLE NAMES
 
-                if line[:3] == postalCode[:3]: #checks the first 3 letters of the postal code and sees if it matches with the file
+                if line[:3] == postalCode[:3]: #checks the first 3 letters of the postal code and sees if it matches with the file (postal_codes.csv)
                     print("Postal Code Validated")
-                    return True #returns true when match
+                    return True #returns true when matches
     
         
         return False #returns false if doesn't match.
     except TypeError:
         print(TypeError)
-        print("Enter a valid postal code")
+        print("Enter a valid postal code") #if it does not match what is inside the file
     
 '''
     This function is to be edited to achieve the task.
@@ -86,62 +88,65 @@ def validatePostalCode(postalCode): #determines whether the postal code is valid
 '''
 def validateCreditCard(creditCard): #determines whether the credit card is valid
     count=1
-    reverseNum= (str(creditCard) [::-1])
+    reverseNum= (str(creditCard) [::-1]) #reverses the inputted number
     sumofOddNumbers=0 
     for num in str(reverseNum): #loop over all the reversed number
             num=int(num)
 
-            if count % 2 ==1:
+            if count % 2 ==1:  #adding each odd index together
                 sumofOddNumbers += num 
             count+=1
 
-    if len(str(creditCard)) < 9:
+    if len(str(creditCard)) < 9: #checks if the given input is less than 9
         return False
     
     try:
-        reversedNumber = reverseNum #FIND HOW TO REVERSE THE NUMBER BY GOOGLE
+        reversedNumber = reverseNum 
         
         sum1 = sumofOddNumbers #add all the odd numbers in the input/string
         sum2 = 0
         finalSum = 0
-
         count = 1
 
         for num in str(reversedNumber): #loop over all the reversed number
             num=int(num)
-
-            if count % 2 ==0:
-                if num * 2 > 9:
-                    for digit in str(num * 2):
+            if count % 2 ==0: #adding each even index together, if it's greater than 2 digits, it adds them together
+                if num * 2 > 9: 
+                    for digit in str(num * 2): #if it's not 2 digits, it will multiply them all together
                         sum2 += int(digit)
                 else:
-                    sum2+= num * 2
+                    sum2+= num * 2 #if it's not 2 digits, it will multiply them all together
             count+=1
 
-        finalSum= sum1+sum2
+        finalSum= sum1+sum2 #adds the sum of the odd numbers and sum of the even numbers together
 
-        if int(str(finalSum) [-1:]) == 0:
+        if int(str(finalSum) [-1:]) == 0: #if the last digit of the final sum is 0, credit card is validated
             print("Credit Card Validated")
             return True 
-        else: 
+        else: #if not, then credit card is invalid.
             print("invalid credit card")
             return False
-            
-    
     except:
         return False
     
-    
-                     
-
-    '''
+'''
     This function is to be edited to achieve the task.
     It is your decision to make this function a procedural or functional type
     You may place as many or as few parameters as needed
     This function may also be broken down further depending on your algorithm/approach
     '''
 def generateCustomerDataFile(): #generates a file with the user's information and their unique ID code    
-    
+    output_file = input("enter output file name: ") #user decides what the file is called
+    filePath = input("Enter a file path: ") #user decides a path to find the file
+    fileName = output_file + ".csv" #names the output file what the user inputted and adds .csv as the file extension
+    fileCreate = filePath + fileName #creates the file
+
+    #opens temporary file and adds what was in the temp file into the file that is created on the user's computer.
+    with open('CustomerData.csv','r') as firstfile, open(fileCreate,'a') as secondfile:
+        # read content from first file
+        for line in firstfile:
+            # append content to second file
+            secondfile.write(line)
 
 
 ####################################################################
